fa1985bb61a0e44c6b51cf36c34135c39ddee636
user <ujjalroy7862@gmail.com>
2023-03-07 17:24:47 +0600

    now knew the simple implementation of the useReducer hook

--- react-project-template/src/components/UseReducerCounter.js
+++ react-project-template/src/components/UseReducerCounter.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
+const initialState = 0;
+const reducer = (state, action) => {
+    switch (action) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        default:
+            return state;
+    }
+};
 const UseReducerCounter = () => {
+    const [count, dispatch] = useReducer(reducer, initialState);
     return (
         <div>
-            <div>Count - 0</div>
-            <button type="button">Increment</button>
-            <button type="button">Decrement</button>
+            <div>Count - {count}</div>
+            <button type="button" onClick={() => dispatch('increment')}>
+                Increment
+            </button>
+            <button type="button" onClick={() => dispatch('decrement')}>
+                Decrement
+            </button>
         </div>
     );
 };
