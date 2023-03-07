44e6294eeb19f2b2cb3777bee2874cf697236389
user <ujjalroy7862@gmail.com>
2023-03-07 17:44:08 +0600

    learn by useReducer Complex example

--- react-project-template/src/components/ComplexUseRedcuer.js
+++ react-project-template/src/components/ComplexUseRedcuer.js
@@ -1,65 +1,78 @@
 import React, { useReducer } from 'react';
 
 const initialState = {
-    counter: 0
+    counter: 0,
+    counter1: 0
 };
 const reducer = (state, action) => {
     switch (action.type) {
         case 'increment':
-            return { counter: state.counter + action.value };
+            return { ...state, counter: state.counter + action.value };
         case 'decrement':
-            return { counter: state.counter - action.value };
+            return { ...state, counter: state.counter - action.value };
+        case 'increment1':
+            return { ...state, counter1: state.counter1 + action.value };
+        case 'decrement1':
+            return { ...state, counter1: state.counter1 - action.value };
         default:
-            return state.counter;
+            return state;
     }
 };
 
 const ComplexUseRedcuer = () => {
     const [count, dispatch] = useReducer(reducer, initialState);
     return (
-        <div>
-            <div>Count - {count.counter}</div>
-            <button
-                type="button"
-                onClick={() =>
-                    dispatch({
-                        type: 'increment',
-                        value: 1
-                    })
-                }>
-                Increment by 1
-            </button>
-            <button
-                type="button"
-                onClick={() =>
-                    dispatch({
-                        type: 'increment',
-                        value: 5
-                    })
-                }>
-                Increment by 5
-            </button>
-            <button
-                type="button"
-                onClick={() =>
-                    dispatch({
-                        type: 'decrement',
-                        value: 1
-                    })
-                }>
-                Decrement by 1
-            </button>
-            <button
-                type="button"
-                onClick={() =>
-                    dispatch({
-                        type: 'decrement',
-                        value: 5
-                    })
-                }>
-                Decrement by 5
-            </button>
-        </div>
+        <>
+            <div>
+                <div>Count - {count.counter}</div>
+                <button
+                    type="button"
+                    onClick={() =>
+                        dispatch({
+                            type: 'increment',
+                            value: 1
+                        })
+                    }>
+                    Increment by 1
+                </button>
+
+                <button
+                    type="button"
+                    onClick={() =>
+                        dispatch({
+                            type: 'decrement',
+                            value: 1
+                        })
+                    }>
+                    Decrement by 1
+                </button>
+            </div>
+
+            <div>
+                <div>Count - {count.counter1}</div>
+                <button
+                    type="button"
+                    onClick={() =>
+                        dispatch({
+                            type: 'increment1',
+                            value: 1
+                        })
+                    }>
+                    Increment by 1
+                </button>
+
+                <button
+                    type="button"
+                    onClick={() =>
+                        dispatch({
+                            type: 'decrement1',
+                            value: 1
+                        })
+                    }>
+                    Decrement by 1
+                </button>
+            </div>
+        </>
     );
 };
 
