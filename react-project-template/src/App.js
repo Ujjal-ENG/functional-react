9f81507f95c2e76737c51f732714d3141a4b8b23
user <ujjalroy7862@gmail.com>
2023-03-07 17:10:47 +0600

    learn new useReducer hoks

--- react-project-template/src/App.js
+++ react-project-template/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import Form from './components/Form';
 
 const App = () => {
-    return (
-        <div>
-            <Form />
-        </div>
-    );
+    return <div></div>;
 };
 
 export default App;
